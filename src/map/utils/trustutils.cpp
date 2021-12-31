a320b0d1b1d500ccb1685cbbf2aae452e380c8c1
Zach Toogood <zrtoogood@gmail.com>
2021-12-31 11:37:31 +0200

    [chore] Clear newly found warnings/errors
    4189: 'scaleOver75': local variable is initialized but not referenced
    4389: '==': signed/unsigned mismatch
    4701: potentially uninitialized local variable
    4702: unreachable code
    4703: potentially uninitialized local pointer variable
    4706: assignment within conditional expression

--- src/map/utils/trustutils.cpp
+++ src/map/utils/trustutils.cpp
@@ -410,7 +410,6 @@ namespace trustutils
         int32 scaleOver60Column = 3;
         int32 scaleOver75Column = 4;
         int32 scaleOver60       = 2;
-        int32 scaleOver75       = 3;
 
         uint8 grade;
 
